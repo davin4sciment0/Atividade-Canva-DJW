012f4117b26b3d9c70ad01ed4d397b8e5a0d811b
davin4sciment0 <davi.nascimento0907@gmail.com>
2023-05-08 11:28:50 -0300

    adição de linhas

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,17 @@
 let can = document.getElementById ('canvas')
 let retangulo = can.getContext ('2d')
 let circulo = can.getContext ('2d')
+let linha = can.getContext ('2d')
 
 retangulo.fillStyle = 'purple'
 retangulo.fillRect (10, 10, 100, 30)
 
 circulo.beginPath()
-circulo.arc(150, 1000, 50, 0, Math.PI * 2)
+circulo.arc(150, 100, 50, 0, Math.PI * 2)
 circulo.lineWidth = 7
-circulo.strokeStyle = 'purple'
+circulo.strokeStyle = 'yellow'
+
+linha.beignPath();
+linha.moveTo(20, 200);
+linha.lineTo(25,250);
+linha.fillStyle = 'black'
