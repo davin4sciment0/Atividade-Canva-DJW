779d2255258364839e0637d0be4413470f3c84ed
Guilherme Matos de Oliveira <127865701+GuilhermeM777@users.noreply.github.com>
2023-05-11 19:01:36 -0300

    Modificação no Arco e adição de comentários.

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,29 @@
 let can = document.getElementById ('canvas');
 let objeto = can.getContext ('2d');
 
+// Criação do Retângulo 
 objeto.fillStyle = 'white';
 objeto.fillRect (10, 10, 100, 30);
 
+// Criação do Círculo
 objeto.beginPath();
 objeto.arc(60, 25, 10, 0, Math.PI * 2);
 objeto.fillStyle = 'red';
 objeto.fill();
 
+// Criação da Linha
 objeto.beginPath();
 objeto.moveTo(15,15);
 objeto.lineTo(10,10);
 objeto.lineTo(125,105);
 objeto.fill();
 
+// Criação do Arco
+objeto.lineWidth = 50;
 objeto.beginPath();
-objeto.moveTo(100,35);
-objeto.quadraticCurveTo(35,35,35,103);
-objeto.fillStyle = 'yellow';
+objeto.moveTo(350, 200);
+objeto.lineTo(450, 150);
+objeto.quadraticCurveTo(250, 300, 400, 300);
+objeto.lineTo(350, 300);
+objeto.strokeStyle = 'black';
 objeto.stroke();
-objeto.fill();
